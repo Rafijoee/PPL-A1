0985782a5e2bdfa3bf7df1ae397ef5d7d52508b4
Rafijoee <rafijoe44@gmail.com>
2024-04-25 05:53:02 +0700

    fix konsultasi petani

--- resources/views/konsultasi/chat.blade.php
+++ resources/views/konsultasi/chat.blade.php
@@ -1,17 +1,7 @@
 @extends('layouts.navbar')
 @extends('layouts.dashboard2')
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-
-<body>
+@extends('layouts.index')
+@section('title', 'Konsultasi')
     <div class="p-4 sm:ml-64 text-justify mt-12">
         <form class="space-y-4 md:space-y-6" method="post" action="/chat" enctype="multipart/form-data">
             @csrf
@@ -63,6 +53,3 @@
             </div>
         </form>
     </div>
-</body>
-
-</html>
